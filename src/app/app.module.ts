5296a0bd9fb34cfc4dc1d818d1e1e26713fb8f02
Reinhard Odhiambo <reinhardodhiambo@gmail.com>
2022-12-18 09:54:26 +0300

    added header layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HeaderModule} from "./shared/layout/header/header.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HeaderModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
